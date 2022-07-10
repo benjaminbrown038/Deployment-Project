efffc62f0a2617749011718542efafbba373322d
Benjamin Brown <35114142+benjaminbrown038@users.noreply.github.com>
2022-07-10 02:40:34 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
-# Deployment-Project on Github
+# Sharpest Minds Project
+Goal:
 
-Will update as deploying more models after building models around data science data
+The ```main.py``` file:
+
+1. Scrape images of cats
+2. Scrape images of dogs
+3. Extract faces from Images
+4. Save images in folder specific to ```cat``` or ```dog``` inside ```Images``` folder
+
+The ```data.py``` file:
+
+1. Create augmented images
+2. save 80% of augmented images to train folder
+3. save 20% of augmented images to test folder
+
+Set up:
+
+1. If not already installed:
+https://www.anaconda.com/products/individual
+2. Create the virtual environment.
+Run: ```conda create -n envname python=3.8 anaconda ```
+3. Clone
+Run: ``` git clone https://www.github.com/benjaminbrown038/Sharpest-Minds-Project.git```
+4. Activate:
+```conda activate envname```
+
+Notes:
+
+  - Must install chrome driver in order to run selenium with chrome webdriver.
+  - Chromedrivers can be found at https://chromedriver.chromium.org/downloads.
+
+References:
+
+  - https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html.
+
+  https://machinelearningmastery.com/how-to-develop-a-convolutional-neural-network-to-classify-photos-of-dogs-and-cats/
+  - MaxPool2D application tutorial: https://www.tutorialspoint.com/how-to-apply-a-2d-max-pooling-in-pytorch
+  - Fitting a model in PyTorch: https://andrewpwheeler.com/2021/05/24/fitting-a-pytorch-model/
+https://www.analyticsvidhya.com/blog/2021/09/convolutional-neural-network-pytorch-implementation-on-cifar10-dataset/
+
+Future work:
+
+- Integrating files to depend on each other
+- Deploying model and data to cloud using AWS and lambda function (Find a couple of reliable tutorials)
+- Running application from terminal using ```python data.py``` while passing classes as parameters
+- Project requires downloading AWS access and secret key in a .csv file and saved in the project directory
---
+++ train.py
@@ -0,0 +1,88 @@
+import torch
+import torchvision
+from torch import optim
+
+# data 
+train = torchvision.datasets.CIFAR10(root='./data', train=True,download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(train, batch_size=batch_size,shuffle=True, num_workers=2)
+# tess
+test = torchvision.datasets.CIFAR10(root='/data',test = True, download = True, transform = transform)
+test_loader = torch.utils.data.DataLoader(test, batch_size = batch_size, shuffle = True, num_workers = 2)
+#classes 
+classes = ('plane','car','bird','cat','deer','dog','frog','horse','ship','truck')
+
+# model cnn architecture                                      
+model = nn.Sequential(
+            
+            nn.Conv2d(3, 32, kernel_size = 3, padding = 1),
+            nn.ReLU(),
+            nn.Conv2d(32,64, kernel_size = 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+        
+            nn.Conv2d(64, 128, kernel_size = 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.Conv2d(128 ,128, kernel_size = 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+            
+            nn.Conv2d(128, 256, kernel_size = 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.Conv2d(256,256, kernel_size = 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+            
+            nn.Flatten(),
+            nn.Linear(82944,1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512,10)
+        )
+print(model)
+
+# hyperparameters (loss and optimizer)
+optimizer = optim.SGD(lr=0.001)
+loss = nn.CrossEntropyLoss()
+
+# training model function
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    for batch, (X, y) in enumerate(dataloader):
+        # Compute prediction and loss
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)   
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+# testing model function            
+def test_loop(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    
+# testing and traing data 
+epochs = 10
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model, loss_fn)
+print("Done!")
+
